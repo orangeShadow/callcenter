be70463729bc5e1f5a5977f17485d885f088471e
Alexeev Anton <alexeev.sker@hmail.com>
2015-03-30 15:19:06 +0000

    edit Exception

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -82,7 +82,7 @@ class PropertyController extends Controller {
             $property= Property::create($request);
             $property->type = $this->propertiesType[$property->type];
             return $property->toJson();
-        }catch(Exceptopn $e)
+        }catch(Exception $e)
         {
             dd($e);
         }
