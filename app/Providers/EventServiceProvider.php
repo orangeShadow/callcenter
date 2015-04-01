bf8133f54793ef72ff9a2effdbfa966dba681826
Alexeev Anton <alexeev.sker@hmail.com>
2015-04-01 09:39:10 +0000

    Edit ::class

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,7 +1,6 @@
 <?php namespace App\Providers;
 
-use App\Events\ClaimCreate;
-use App\Handlers\Events\SendMailToClient;
+use App\Handlers\Events;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -17,8 +16,8 @@ class EventServiceProvider extends ServiceProvider {
 			'EventListener',
 		],
 
-        ClaimCreate::class =>[
-            SendMailToClient::class
+        'App\Events\ClaimCreate'=>[
+            'App\Handlers\Events\SendMailToClient'
         ]
 	];
 
