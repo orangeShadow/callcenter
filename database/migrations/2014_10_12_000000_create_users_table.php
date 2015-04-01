a8e36fb50e7594ad181daae17da8958b589a5f1b
Alexeev Anton <alexeev.sker@hmail.com>
2015-04-01 10:13:55 +0000

    edit password length

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration {
 			$table->increments('id');
 			$table->string('name');
 			$table->string('email')->unique();
-			$table->string('password', 60);
+			$table->string('password', 120);
             $table->string('phone',50)->nullable();
             $table->integer('role_id')->unsigned()->default(4);
 			$table->rememberToken();
