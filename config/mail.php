21ef7ba887d3d71f95e246ba4900a4dbaabdf962
melyfaro <alexeev.sker@gmail.com>
2015-04-08 10:58:42 +0000

    Edd from

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => "callcenter1@roumingu.net", 'name' => "CallCenter№1"],
 
 	/*
 	|--------------------------------------------------------------------------
