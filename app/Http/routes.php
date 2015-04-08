2c4c4d7cfa8532beae5b42aa72e91b6f49d13159
melyfaro <alexeev.sker@gmail.com>
2015-04-08 10:01:36 +0000

    remove middleware auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -41,14 +41,27 @@ Route::get('/', function(Request $request){
 Route::resource('project','ProjectController');
 
 Route::resource('claim','ClaimController');
-Route::post('claim/statuschange',['middleware'=>'auth',function(){
+Route::post('claim/statuschange',function(Request $request){
+    if (Auth::guest())
+    {
+        if ($request::ajax())
+        {
+            return response('Unauthorized.', 401);
+        }
+        else
+        {
+            return redirect()->guest('auth/login');
+        }
+    }
+
     $id  = Request::get('id');
     $claim =App\Claim::findOrFail($id);
     $claim->status = Request::get('status');
     $claim->note = Request::get('note');
     $claim->save();
     return redirect("claim/$id");
-}]);
+
+});
 
 
 
