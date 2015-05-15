12cf1665de95d11acbee7d9ed8f6d6c9eee90e6a
melyfaro <alexeev.sker@gmail.com>
2015-05-15 16:36:57 +0000

    Edit style form

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -203,35 +203,10 @@ Route::get('externform',function(){
        ";
 
     $script = "
-        <script>
-            function cpopupClose(){
-                document.getElementById(\"cc-popup\").style.display=\"none\";
-                document.getElementById(\"cc-popup-shadow\").style.display=\"none\";
-            }
-            function cSendCall(){
-                document.getElementById(\"cc-error\").innerText=\"\";
-                var phone =document.getElementById(\"cc-phone\").value;
-                if(!/^\\\\+?[0-9]{11,12}$/.test(phone)){
-                    document.getElementById(\"cc-error\").innerText=\"Неверный формат телефона, должны быть только цифры\";
-                    return false;
-                }
-                document.getElementById(\"cc-call\").style.display = \"none\";
-                document.getElementById(\"cc-call1\").style.display = \"inline-block\";
-                var r = new XMLHttpRequest();
-                r.open(\"GET\",\"".url('externcall')."?phone=\"+phone, true);
-                r.onreadystatechange = function () {
-                    if (r.readyState != 4 || r.status != 200) return;
-                };
-                r.send();
-            }
-            ;(function(){
-                var body = document.body;
-                var html = document.documentElement;
-                var height = Math.max( body.scrollHeight, body.offsetHeight,html.clientHeight, html.scrollHeight, html.offsetHeight );
-                $(\"#cc-popup-shadow\").css(\"height\",height);
-                console.log(height);
-            })()
-        </script>
+            var body = document.body;
+            var html = document.documentElement;
+            var height = Math.max( body.scrollHeight, body.offsetHeight,html.clientHeight, html.scrollHeight, html.offsetHeight );
+            $(\"#cc-popup-shadow\").css(\"height\",height);
         ";
 
     $html = '
@@ -239,7 +214,7 @@ Route::get('externform',function(){
         <div id="cc-popup">
         <div class="cc-close" onclick="cpopupClose()"></div>
         <div class="cc-content">
-            <h1>Оставьте свой номер и мы перезвоним Вам<br> в течении <strong>30</strong> секунд!</h1>
+            <h1>Оставьте свой номер и мы перезвоним Вам<br> в течении <strong id="cc-timer">30</strong> <strong>секунд</strong>!</h1>
             <div class="wrapper">
                 <input id="cc-phone" type = "text" value="+7" placeholder="+7"> <a id="cc-call" onClick="cSendCall()" href="#">Жду звонка</a><a id="cc-call1" href="#">Жду звонка</a>
                 <div id="cc-error"></div>
@@ -256,10 +231,43 @@ Route::get('externform',function(){
     $html = trim(preg_replace('/\s\s+/', '', $html));
     $script = trim(preg_replace('/\s\s+/', '', $script));
 
-    return ";(function(){
+    return "
+            function cpopupClose(){
+                document.getElementById(\"cc-popup\").style.display=\"none\";
+                document.getElementById(\"cc-popup-shadow\").style.display=\"none\";
+            }
+            function cSendCall(){
+                document.getElementById(\"cc-error\").innerText=\"\";
+                var phone =document.getElementById(\"cc-phone\").value;
+                console.log(/^\\+?[0-9]{11,12}$/.test(phone));
+                if(!/^\\+?[0-9]{11,12}$/.test(phone)){
+                    document.getElementById(\"cc-error\").innerText=\"Неверный формат телефона, должны быть только цифры\";
+                    return false;
+                }
+                document.getElementById(\"cc-call\").style.display = \"none\";
+                document.getElementById(\"cc-call1\").style.display = \"inline-block\";
+                setTimeout(timerDown,1000);
+                /*
+                var r = new XMLHttpRequest();
+                r.open(\"GET\",\"".url('externcall')."?phone=\"+phone, true);
+                r.onreadystatechange = function () {
+                    if (r.readyState != 4 || r.status != 200) return;
+
+                };
+                r.send();
+                */
+            }
+
+            function timerDown(){
+                var time = document.getElementById(\"cc-timer\").innerText;
+                document.getElementById(\"cc-timer\").innerText = time-1;
+                if(time>=0) setTimeout(timerDown,1000);
+            }
+            ;(function(){
                 window.setTimeout(function(){
-                    $('body').append('".$style.$html.$script."');
+                    document.body.innerHTML+='".$style.$html."';
                 }, 3000);
+                ".$script.";
             })();";
 });
 
