51783c29b9e7d5176d9ed9be48f46582ce6611e3
Anton Alexeev <alexeev.sker@gmail.com>
2016-05-24 16:23:10 +0300

    Логирование отправки, перехват ошибки при создании заявки

--- app/Handlers/Events/SendMailToClient.php
+++ app/Handlers/Events/SendMailToClient.php
@@ -25,63 +25,68 @@ class SendMailToClient {
 	 */
 	public function handle(ClaimCreate $event)
 	{
-        $claim = $event->claim;
-        $properties = \App\Property::showPropertyValue($claim);
+        try{
+            $claim = $event->claim;
+            $properties = \App\Property::showPropertyValue($claim);
 
-        $send = 1;
-        $rType = $event->claim->typeR;
-        if(!empty($rType))
-        {
-            $send = $rType->send_mail;
-        }
-        if($send==1)
-        {
-            \Mail::send('emails.claimcreate',compact('claim','properties'), function($message) use ($event)
+            $send = 1;
+            $rType = $event->claim->typeR;
+            if(!empty($rType))
             {
-                $emails = [];
-                $emails[] = $event->claim->project->client->email;
-                if(!empty($event->claim->project->client->send_email)){
-                    $emailsSplit  = explode(",",$event->claim->project->client->send_email);
-                    foreach($emailsSplit as $item)
-                    {
-                        $emails[] = trim($item);
+                $send = $rType->send_mail;
+            }
+            if($send==1)
+            {
+                \Mail::send('emails.claimcreate',compact('claim','properties'), function($message) use ($event)
+                {
+                    $emails = [];
+                    $emails[] = $event->claim->project->client->email;
+                    if(!empty($event->claim->project->client->send_email)){
+                        $emailsSplit  = explode(",",$event->claim->project->client->send_email);
+                        foreach($emailsSplit as $item)
+                        {
+                            $emails[] = trim($item);
+                        }
                     }
-                }
-                //Log::alert('Отправка письма '.$event->claim->id.", email:".$event->claim->project->client->send_email.", ".$event->claim->project->client->email);
-                $res = $message->to($emails, 'Callcenter №1')->subject('Круглосуточный call-центр №1')->from('robot@goodline.ru');
-                //Log::info(json_encode($res));
-            });
-        }
+                    //Log::alert('Отправка письма '.$event->claim->id.", email:".$event->claim->project->client->send_email.", ".$event->claim->project->client->email);
+                    $res = $message->to($emails, 'Callcenter №1')->subject('Круглосуточный call-центр №1')->from('robot@goodline.ru');
+                    //Log::info(json_encode($res));
+                });
+            }
 
-        if(!empty($event->claim->missed_call))
-        {
-            \Mail::send('emails.fail',compact('claim','properties'), function($message) use ($event)
+            if(!empty($event->claim->missed_call))
             {
-                $emails = [];
-                $emails[] = $event->claim->project->client->email;
-                if(!empty($event->claim->project->client->send_email)){
-                    $emailsSplit  = explode(",",$event->claim->project->client->send_email);
-                    foreach($emailsSplit as $item)
-                    {
-                        $emails[] = trim($item);
+                \Mail::send('emails.fail',compact('claim','properties'), function($message) use ($event)
+                {
+                    $emails = [];
+                    $emails[] = $event->claim->project->client->email;
+                    if(!empty($event->claim->project->client->send_email)){
+                        $emailsSplit  = explode(",",$event->claim->project->client->send_email);
+                        foreach($emailsSplit as $item)
+                        {
+                            $emails[] = trim($item);
+                        }
                     }
-                }
-                if(!empty($event->destinations)){
-                    $emails[] = $event->destinations;
-                }
+                    if(!empty($event->destinations)){
+                        $emails[] = $event->destinations;
+                    }
 
-                $message->to($emails, 'Callcenter №1')->subject('Круглосуточный call-центр №1')->from('robot@goodline.ru');
-            });
-        }
+                    $message->to($emails, 'Callcenter №1')->subject('Круглосуточный call-центр №1')->from('robot@goodline.ru');
+                });
+            }
 
 
-        if(!empty($event->destinations))
-        {
-            \Mail::send('emails.claimcreate',compact('claim','properties'), function($message) use ($event)
+            if(!empty($event->destinations))
             {
-                $message->to($event->destinations, 'Callcenter №1')->subject('Круглосуточный call-центр №1')->from('robot@goodline.ru');
-            });
+                \Mail::send('emails.claimcreate',compact('claim','properties'), function($message) use ($event)
+                {
+                    $message->to($event->destinations, 'Callcenter №1')->subject('Круглосуточный call-центр №1')->from('robot@goodline.ru');
+                });
+            }
+        }catch(\Exception $e){
+            \Log::error('Ошибка при создании заявки',array('error'=>$e->getMessage()));
         }
+
 
 	}
 
