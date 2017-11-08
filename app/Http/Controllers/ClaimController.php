3f2a730d6175e78ffdfce4ceffca731aea7d575a
Anton Alexeev <alexeev.sker@gmail.com>
2017-11-08 16:20:01 +0300

    Уменьшил отображение

--- app/Http/Controllers/ClaimController.php
+++ app/Http/Controllers/ClaimController.php
@@ -36,9 +36,9 @@ class ClaimController extends Controller {
 	{
         $fluid = true;
         if(Auth::user()->checkRole('client')){
-            $claims = Claim::client(Request::all())->orderBy('created_at','desc')->paginate(30);
+            $claims = Claim::client(Request::all())->orderBy('created_at','desc')->paginate(10);
         }else{
-            $claims = Claim::search(Request::all())->orderBy('created_at','desc')->paginate(30);
+            $claims = Claim::search(Request::all())->orderBy('created_at','desc')->paginate(10);
         }
 
 		return view('claim.index',compact('claims','fluid'));
